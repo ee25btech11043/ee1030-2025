1910b85ffc1892e87ec0e150f3a9bd691114bf2b
ee25btech11043 <ee25btech11043@iith.ac.in>
2025-10-18 11:09:01 +0530

    Update code23.c

--- ee25btech11043/matgeo/12.472/codes/code23.c
+++ ee25btech11043/matgeo/12.472/codes/code23.c
@@ -8,6 +8,17 @@ double calculate_horizontal_force(double weight, double angle_degrees) {
     // Convert angle from degrees to radians for trigonometric functions
     double angle_radians = angle_degrees * M_PI / 180.0;
 
+   // In equilibrium, summing forces in the vertical direction:
+    // T * cos(angle_radians) = weight
+    // So, Tension T = weight / cos(angle_radians)
+
+    // Summing forces in the horizontal direction:
+    // F = T * sin(angle_radians)
+
+    // Substitute T into the equation for F:
+    // F = (weight / cos(angle_radians)) * sin(angle_radians)
+    // F = weight * tan(angle_radians)
+    
     // F = W * tan(angle)
     double force_F = weight * tan(angle_radians);
 
